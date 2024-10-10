bfbcc69ffd5ce06e255ef45da583744b87103822
sheha_mgcb38b <shehabkh249@gmail.com>
2024-10-10 18:42:02 +0200

    added a new test line for the new function

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,10 @@
 
 public class Main {
     public static void main(String[] args) {
-//        Adder adder = new Adder();
-//        System.out.println(adder.add(1, 2));
-//
+       Adder adder = new Adder();
+       System.out.println(adder.add(1, 2));
+        System.out.println(adder.addntimes(2,2,3));
+
         Subtractor subtractor = new Subtractor();
         System.out.println(subtractor.subtract(6, 3));
         }
