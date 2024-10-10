c8c4096acdfce940a16a5973ebd03411c7ff8eed
sheha_mgcb38b <shehabkh249@gmail.com>
2024-10-10 18:29:50 +0200

    added a new function

--- src/Adder.java
+++ src/Adder.java
@@ -5,4 +5,13 @@ public class Adder {
     public int add(int a, int b) {
         return a + b;
     }
+
+    public int addntimes(int a,int b,int n){
+        int i = 0;
+        int sum = 0;
+        while (i < n){
+            sum = sum + (a+b);
+        }
+        return sum;
+    }
 }
